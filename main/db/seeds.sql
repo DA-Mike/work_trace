acb63926ee3a902f55106045f1c43be5fe0a2620
Mike Rugh <mike.rugh@gmail.com>
2022-09-07 20:16:20 -0700

    adds seeds

--- main/db/seeds.sql
+++ main/db/seeds.sql
@@ -11,16 +11,17 @@ VALUES  ("Engineer", 150000, 1),
         ("Accountant", 80000, 3),
         ("Marketing Associate", 95000, 4),
         ("HR Analyst", 25000, 5),
-        ("Manager", 200000, 1),
-        ("Manager", 175000, 2),
-        ("Manager", 170000, 3),
-        ("Manager", 150000, 4),
-        ("Manager", 50000, 5);
+        ("Engineering Manager", 200000, 1),
+        ("Finance Manager", 175000, 2),
+        ("Accounting Manager", 170000, 3),
+        ("Marketing Manager", 150000, 4),
+        ("HR Manager", 50000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ("Joe", "Schmoe", 1, 6),
-        ("Valerie", "Mallory", 2, NULL),
+        ("Valerie", "Mallory", 2, 7),
         ("Bean", "Counter", 3, NULL),
         ("Bubble", "Wubbles", 4, NULL),
         ("Karen", "Smith", 5, NULL),
         ("Tony", "Soprano", 6, NULL);
+        ("Tim Grim", 7, NULL)
